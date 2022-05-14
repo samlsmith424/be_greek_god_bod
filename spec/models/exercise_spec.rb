b51fa504b486b0766626aba47ea797e3808c304f
Sully Birashk <sully.birashk@gmail.com>
2022-05-14 11:39:57 -0700

    Feat: Exercise Model Test

---
+++ spec/models/exercise_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Exercise, type: :model do
+
+ describe 'relationships' do
+  it { should have_many :workout_exercises }
+  it { should have_many(:workouts).through(:workout_exercises) }
+ end
+
+ describe 'validations' do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:muscle_group) }
+ end
+end
