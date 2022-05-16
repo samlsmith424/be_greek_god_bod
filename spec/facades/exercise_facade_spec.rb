ace35ace819a20e16e8e66c1576814332830f667
Sam Smith <samlsmith424@gmail.com>
2022-05-15 18:58:11 -0700

    Test: Exercise facade returns exercise objects

---
+++ spec/facades/exercise_facade_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'ExerciseFacade' do
+
+  it 'returns example' do
+    VCR.use_cassette("many jobs") do
+      exercises = ExerciseFacade.find_all_exercises
+
+      expect(exercises).to be_an(Array)
+      exercises.each do |exercise|
+        expect(exercise).to be_an(ExercisePoro)
+        expect(exercise.body_part).to be_a(String)
+        expect(exercise.equipment).to be_a(String)
+        expect(exercise.gif_url).to be_a(String)
+        expect(exercise.id).to be_a(String)
+        expect(exercise.target_muscle).to be_a(String)
+      end
+    end
+  end
+end
