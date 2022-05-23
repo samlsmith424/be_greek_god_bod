3bb82019bda8b40bafe46dc154b5b05296c00803
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-05-23 18:27:01 -0400

    Feat: add exercise relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :workouts
+  has_many :workout_exercises, through: :workouts
+  has_many :exercises, through: :workout_exercises
+
   validates_presence_of :name
   validates_presence_of :password
 
