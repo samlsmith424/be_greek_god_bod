f7533eb12e5db06223d74845a3606db065cfb9b4
Sully Birashk <sully.birashk@gmail.com>
2022-05-24 15:44:59 -0700

    Feat: Add Name Column to Messages Table

--- db/migrate/20220524172352_create_messages_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateMessagesTable < ActiveRecord::Migration[5.2]
-  def change
-    create_table :messages_tables do |t|
-      t.text :content
-      t.references :user, foreign_key: true
-    end
-  end
-end
