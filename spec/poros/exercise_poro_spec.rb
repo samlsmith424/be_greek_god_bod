d081a6aaee29f05162e93686cc0ff4219275592c
Sam Smith <samlsmith424@gmail.com>
2022-05-25 14:59:50 -0700

    Test: exercise poro returns empty array if there are no intervals

--- spec/poros/exercise_poro_spec.rb
+++ spec/poros/exercise_poro_spec.rb
@@ -8,8 +8,15 @@ RSpec.describe 'Exercise Poro' do
 
     expect(exercise).to be_a(ExercisePoro)
     expect(exercise.equipment).to eq("body weight")
-    expect(exercise.gif).to eq("http://d205bpvrqc9yn1.cloudfront.net/0001.gif")
-    expect(exercise.id).to eq("0001")
+    expect(exercise.gif_url).to eq("http://d205bpvrqc9yn1.cloudfront.net/0001.gif")
+    expect(exercise.id).to be_an(Integer)
     expect(exercise.name).to eq("3/4 sit-up")
+  end
+
+  it "returns an empty array if there are no intervals" do
+    data = Exercise.create(equipment: "body weight", gif: "http://d205bpvrqc9yn1.cloudfront.net/0001.gif", name: "3/4 sit-up")
+
+    exercise = ExercisePoro.new(data)
+    expect(exercise.intervals).to eq([])
   end
 end
