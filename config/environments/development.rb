83f5217b3c3277ae21a7cd9f0f7727eb39299640
Sully Birashk <sully.birashk@gmail.com>
2022-05-25 14:10:30 -0700

    Configure Heroku Link!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.action_cable.url = "ws://localhost:5000/cable"
+  config.action_cable.url = "wss://be-greek-god-bod.herokuapp.com/cable"
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
