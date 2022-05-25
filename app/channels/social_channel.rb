e65efc29cb43693e4ecb2d3ec636b57bc770427e
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-05-25 12:42:19 -0400

    Refactor: change chat channel to social channel

---
+++ app/channels/social_channel.rb
@@ -0,0 +1,11 @@
+class SocialChannel < ApplicationCable::Channel
+  def subscribed
+    social = Social.first
+    stream_for social
+# stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
