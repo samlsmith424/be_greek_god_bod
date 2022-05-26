6b5c5d5a2109d5ec8d20c52b1fcb88961b296b76
Sam Smith <samlsmith424@gmail.com>
2022-05-25 18:20:30 -0700

    Test: social model validation

--- spec/models/social_spec.rb
+++ spec/models/social_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Social, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+     it { should have_many :messages }
+   end
 end
