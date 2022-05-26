b1f29816c72ff752ca2d6b215c1b40eb24b1bc4a
Sam Smith <samlsmith424@gmail.com>
2022-05-25 18:20:55 -0700

    Refactor: remove unnecessary code

--- app/controllers/api/v1/exercises_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::ExercisesController < ApplicationController
-  def index
-    exercises = ExerciseFacade.find_all_exercises #(params[:all])
-    # render json: ExerciseSerializer.new(exercises)
-  end
-end
